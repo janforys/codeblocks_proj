7d7ebed0121251042d5be22bb3423a6a9587b1cf
janforys <janforys@wp.pl>
2022-03-15 23:46:44 +0000

    leap Y

--- C/Loops/leap_year.c
+++ C/Loops/leap_year.c
@@ -2,21 +2,17 @@
 
 int main()
 {
-    int base_year=2020, user_year;
-    scanf("%d",&user_year);
-    printf("Base: %d\nUser: %d\n",base_year,user_year);
+    int year;
+    printf("Enter a year to check if it is leap or not: ");
+    scanf("%d",&year);
 
-    int diff=(user_year-base_year)/4;
-    int base_leap=(base_year/4+diff);
-    printf("%d\n",base_leap);
-
-    if (user_year/4==base_leap)
+    if (year%4==0)
         {
-            printf("This is leap year.\n");
+            printf("%d is a leap year\n",year);
         }
     else
         {
-            printf("This isn't leap year.\n");
+            printf("%d isn't a leap year\n",year);
         }
 
     return 0;
