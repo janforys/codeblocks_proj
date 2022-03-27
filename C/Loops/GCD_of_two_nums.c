4dd214711292b4e109b3534d28f8ef8e98b15311
janforys <janforys@wp.pl>
2022-03-27 07:59:08 +0100

    GCD update

--- C/Loops/GCD_of_two_nums.c
+++ C/Loops/GCD_of_two_nums.c
@@ -2,21 +2,38 @@
 
 int main()
 {
-    int a=30,b=36,c;
-    //scanf("%d%d",&a,&b);
-    do {
+    int a,b,c,d,e;
+
+    printf("Type A: ");
+    scanf("%d",&a);
+    printf("Type B: ");
+    scanf("%d",&b);
+
+    //swap "a" with "b"
+    if (a<b)
+    {
+        int temp;
+        temp=a;
+        a=b;
+        b=temp;
+    }
+
+    c=a%b;
+    printf("%d\n",c);
+    d=b%c;
+    printf("%d\n",d);
+    e=c%d;
+    printf("%d\n\n",e);
+
+    printf("GCD = %d\n",d);
+
+    /*do {
     c=a%b;
     a=b;
     b=c;
     }
     while(b==0);
-    printf("%d\n",a);
-
-       /* if (b==0) {
-            printf("%d",a);
-        }
-        else
-            printf("go to 1");*/
+    printf("%d\n",a);*/
 
     return 0;
 }
