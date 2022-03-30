39f5b27dc1a9388a58d1043e9893b6dfb0bfea75
janforys <janforys@wp.pl>
2022-03-30 10:01:46 +0100

    GCD update

--- C/Loops/GCD_of_two_nums.c
+++ C/Loops/GCD_of_two_nums.c
@@ -5,36 +5,39 @@ do not work for all numbers */
 
 int main()
 {
-    int a,b,c,d,e;
+    int a,b,c,d;
 
     printf("Type A: ");
     scanf("%d",&a);
     printf("Type B: ");
     scanf("%d",&b);
 
-    //swap "a" with "b"
+    //swap "a" with "b" if a<b
     if (a<b)
     {
-        int temp;
-        temp=a;
+        int temp=a;
         a=b;
         b=temp;
     }
 
+    do {
     c=a%b;
-    //printf("%d\n",c);
-    d=b%c;
-    //printf("%d\n",d);
 
-    if (d==0) {
-        printf("\nGCD = %d\n",c);
+    if (c==0) {
+        printf("\nGCD = %d\n",b);
+        break;
         }
-    else
-    {
-        e=c%d;
-        //printf("%d\n\n",e);
+    else {
+        //The problem is with 4 lines below...
+        //...inside "else" statement.
+        int temp=b;
+        a=temp;
+        b=c;
+        d=a%b;
         printf("\nGCD = %d\n",d);
+        }
     }
+    while (d==0);
 
     return 0;
 }
