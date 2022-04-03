c5f5da7c5098a7950ec307c698dbe6a37545ea0f
janforys <janforys@wp.pl>
2022-04-03 15:23:35 +0200

    LCM Update

--- C/Loops/LCM_of_two_nums.c
+++ C/Loops/LCM_of_two_nums.c
@@ -5,7 +5,7 @@
 
 int main()
 {
-    int n1,n2,k,LCM;
+    int n1,n2,n,m;
 
     printf("To find LCM please give me 2 numbers: \n");
     printf("1st: ");
@@ -13,12 +13,22 @@ int main()
     printf("2nd: ");
     scanf("%d",&n2);
 
-    for (k; k<=n2; k++)
+    // nested "for" loop is not working as it should
+    for (int k=1; k<=n1; k++)
     {
-        LCM=n1*k;
+            n=k*n1;
+            printf("%d\n", n);
+
+        for (int j=1; j<=n2; j++)
+        {
+            m=j*n2;
+            printf("%d\n", m);
+        }
     }
 
-    printf("\nLCM = %d\n", LCM);
+    if (n==m) {
+            printf("\nLCM = %d\n", n);
+        }
 
     return 0;
 }
