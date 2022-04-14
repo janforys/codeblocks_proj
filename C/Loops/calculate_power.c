fb5057d15ea75d10676a199b8aa4f7cad5efea07
janforys <janforys@wp.pl>
2022-04-14 22:17:51 +0200

    Calculate Power
    Initial

---
+++ C/Loops/calculate_power.c
@@ -0,0 +1,17 @@
+/* C Program to calculate power of two numbers entered by a user */
+
+#include <stdio.h>
+
+int main()
+{
+int b,p,x=1,z;
+scanf("%d%d",&b,&p);
+b*=b;
+while(x<p)
+{
+r=b*z;
+x++;
+z++;
+}
+return 0;
+}
