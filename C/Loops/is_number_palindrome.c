2da0f99d3ab40c069bb1fcffa1bc34bb31b3da77
janforys <janforys@wp.pl>
2022-04-22 22:30:51 +0200

    Some changes

--- C/Loops/is_number_palindrome.c
+++ C/Loops/is_number_palindrome.c
@@ -10,18 +10,21 @@ int main()
     scanf("%d",&n);
     printf("\nYou've entered %d...", n);
 
+    int m=n;
+
     // reverse n
     do {
+
         p=n%10;
         n=n/10;
         printf("%d",p);
-        x=p;
+        //p=p+p;
     } while (n>=1);
 
-    printf("\n\n%d\n", x); // show "p"(x)
+    printf("\n\n%d\n%d\n", m, p); // show p and m(n)
 
     // check if n is not equal to p
-    if (n/=p) {
+    if (m/=p) {
         printf("\n ...and it is not a palindrome.\n\n");
     }
         printf("\n ...and it is palindrome number.\n\n");
