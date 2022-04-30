88667c60529f23c1c0860e2a8a58f06f6637eb02
janforys <janforys@wp.pl>
2022-04-30 22:15:36 +0200

    is_prime.c completed and working

--- C/Loops/is_prime.c
+++ C/Loops/is_prime.c
@@ -6,37 +6,32 @@ int main()
 {
     // Keep in mind that amount of prime numbers is endless...
     // Prime number is a positive integer
+    // Remember that every number can be divided by 1 and itself, so
+    //you have to exclude everything beside 1 and your number with
+    //help of loop.
+    // Simply check by using loop that it divides by other numbers :)
 
-    int a;
+    int user_num, temp=0;
 
     // Enter number
     printf("Enter a positive number: ");
-    scanf("%d", &a);
+    scanf("%d", &user_num);
 
-    // Check if it divides ONLY by itself and by 1 with no remainder
-    for (int x=2; x<a; x++) {
+    // Check if it divides by other than itself and 1
+    for (int x=2; x < user_num; x++) {
 
-        // that condition do not work for number 9
-        /*if (a % x != 0) {
-            continue;
-            //printf("IT'S PRIME!\n");
-        }*/
-
-        if (a % x == 0) {
-            printf("IT IS NOT PRIME!\n");
-            break;
-        }
-        else
-        {
-            //continue;
-            printf("IT'S PRIME!\n");
+        if (user_num % x == 0) {
+            temp = 1;
             break;
         }
     }
-    // Remember that every number can be divided by 1 and itself, so
-    //you have to exclude everything beside 1 and your number with
-    //help of loop
-    // Simply check by using loop that it divides by other numbers :)
+
+    if (temp == 1) {
+        printf("IT IS NOT PRIME.\n");
+    }
+    else {
+        printf("IT'S PRIME!\n");
+    }
 
 return 0;
 }
