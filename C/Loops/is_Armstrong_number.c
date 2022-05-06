973cde745b50ecb72b25242a7ba1d220dedcbedd
janforys <janforys@wp.pl>
2022-05-06 10:07:44 +0200

    New example.

---
+++ C/Loops/is_Armstrong_number.c
@@ -0,0 +1,27 @@
+/* C Program to Check Armstrong Number.
+
+   User enters a number.
+   The program need to check if it is armstrong number or not.
+
+   Divide number to digits (use modulo operator).
+   Check if all digits have been used (does amount matter?).
+   Raise each digit to the power of n, where: n is amount of digits.
+   Sum powered digits.
+   Compare result with number entered by a user.
+
+   e.g.: abcd... = a^n + b^n + c^n + d^n + */
+
+
+#include <stdio.h>
+
+int main()
+{
+    // Declare variables
+    int num_to_check;
+
+    printf("Enter positive number: ");
+    scanf("%d", &num_to_check);
+
+
+return 0;
+}
