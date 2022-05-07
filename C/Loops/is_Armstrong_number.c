ac21d65647cfcd43ecd6c7489cfd2d325ee026f2
janforys <janforys@wp.pl>
2022-05-07 10:46:04 +0200

    update

--- C/Loops/is_Armstrong_number.c
+++ C/Loops/is_Armstrong_number.c
@@ -11,17 +11,43 @@
 
    e.g.: abcd... = a^n + b^n + c^n + d^n + */
 
-
 #include <stdio.h>
 
 int main()
 {
     // Declare variables
-    int num_to_check;
+    int num_to_check, digit, power_factor;
 
     printf("Enter positive number: ");
     scanf("%d", &num_to_check);
 
+    // Counting power factor (n)
+    /*int a = num_to_check;
+    while (a >= 1) {
+        a /= 10;
+        power_factor++;
+    }*/
+
+    // Counting power factor (n)
+    for (int a = num_to_check; a >= 1; a /= 10) {
+        power_factor++;
+    }
+    printf("POWER FACTOR = %d\n", power_factor);
+
+    // some comment
+    while (num_to_check >= 1) {
+        digit = num_to_check % 10;
+        printf("%d\n", digit);
+
+            int digit2;
+            for (int i=0; i <= power_factor; i++)
+            {
+                digit2 = digit * i;
+            }
+            printf("%d\n\n", digit2);
+
+        num_to_check /= 10;
+    }
 
 return 0;
 }
