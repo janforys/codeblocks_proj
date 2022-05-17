1fd68527cc809e2f034a0eb84563df9bc875da7c
janforys <janforys@wp.pl>
2022-05-17 12:21:19 +0200

    New task

---
+++ C/Loops/patterns.c
@@ -0,0 +1,51 @@
+/* C Program to Print Pyramids and Patterns */
+
+/* Switch..case statement with different program choose
+   options:
+   Half pyramid of *, half pyramid of numbers, half
+   pyramid of alphabets, Inverted half pyramid of *,
+   Inverted half pyramid of numbers, Full pyramid of *,
+   Full pyramid of numbers, Inverted full pyramid of *,
+   Pascal's Triangle, Floyd's Triangle. */
+
+#include <stdio.h>
+
+int main()
+{
+    int example;
+
+    switch (example) {
+
+        case 1:
+        break;
+
+        case 2:
+        break;
+
+        case 3:
+        break;
+
+        case 4:
+        break;
+
+        case 5:
+        break;
+
+        case 6:
+        break;
+
+        case 7:
+        break;
+
+        case 8:
+        break;
+
+        case 9:
+        break;
+
+        case 10:
+        break;
+    }
+
+return 0;
+}
