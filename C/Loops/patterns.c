e88a00b5ba4f7a75f56a1ffa9b92d67dc143e14f
janforys <janforys@wp.pl>
2022-05-17 12:47:06 +0200

    update patterns.c

--- C/Loops/patterns.c
+++ C/Loops/patterns.c
@@ -2,11 +2,12 @@
 
 /* Switch..case statement with different program choose
    options:
-   Half pyramid of *, half pyramid of numbers, half
+   Half pyramid of *, Half pyramid of numbers, Half
    pyramid of alphabets, Inverted half pyramid of *,
    Inverted half pyramid of numbers, Full pyramid of *,
    Full pyramid of numbers, Inverted full pyramid of *,
    Pascal's Triangle, Floyd's Triangle. */
+
 
 #include <stdio.h>
 
@@ -14,37 +15,47 @@ int main()
 {
     int example;
 
-    switch (example) {
+    printf("Choose example (1-10): ");
+    scanf("%d", &example);
 
-        case 1:
-        break;
+    while (1) {
+        if (example < 11) {
+            switch (example) {
 
-        case 2:
-        break;
+                case 1: printf("Half pyramid of *\n");
+                break;
 
-        case 3:
-        break;
+                case 2: printf("Half pyramid of numbers\n");
+                break;
 
-        case 4:
-        break;
+                case 3: printf("Half pyramid of alphabets\n");
+                break;
 
-        case 5:
-        break;
+                case 4: printf("Inverted half pyramid of *\n");
+                break;
 
-        case 6:
-        break;
+                case 5: printf("Inverted half pyramid of numbers\n");
+                break;
 
-        case 7:
-        break;
+                case 6: printf("Full pyramid of *\n");
+                break;
 
-        case 8:
-        break;
+                case 7: printf("Full pyramid of numbers\n");
+                break;
 
-        case 9:
-        break;
+                case 8: printf("Inverted full pyramid of *\n");
+                break;
 
-        case 10:
-        break;
+                case 9: printf("Pascal's Triangle\n");
+                break;
+
+                case 10: printf("Floyd's Triangle\n");
+                break;
+
+                //default: printf("Choose another\n");
+                }
+        }
+
     }
 
 return 0;
