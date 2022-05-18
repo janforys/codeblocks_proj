7af861781f290dc4da1c1b04acbfdb7de1dae9f1
janforys <janforys@wp.pl>
2022-05-18 23:25:16 +0200

    case 1 in patterns.c

--- C/Loops/patterns.c
+++ C/Loops/patterns.c
@@ -15,48 +15,62 @@ int main()
 {
     int example;
 
-    printf("Choose example (1-10): ");
+    label: // goto
+    printf("Choose an example (1-10): ");
     scanf("%d", &example);
 
-    while (1) {
-        if (example < 11) {
-            switch (example) {
+    switch (example) {
 
-                case 1: printf("Half pyramid of *\n");
+        case 1: printf("[ Half pyramid of * ]\n");
+                int stars, rows;
+                printf("How many stars ?: ");
+                scanf("%d", &stars);
+                rows = stars;
+                int col = rows;
+                printf("\n");
+
+                // Rows (stars) iterator
+                for (int i = 0; i < rows; i++) {
+                        // Column iterator
+                        for (int j = 0; j < col; j++) {
+                            printf("* ");
+                            }
+                        printf("\n");
+                    }
+
+                //printf("\n");
                 break;
 
-                case 2: printf("Half pyramid of numbers\n");
+        case 2: printf("[ Half pyramid of numbers ]\n");
                 break;
 
-                case 3: printf("Half pyramid of alphabets\n");
+        case 3: printf("[ Half pyramid of alphabets ]\n");
                 break;
 
-                case 4: printf("Inverted half pyramid of *\n");
+        case 4: printf("[ Inverted half pyramid of * ]\n");
                 break;
 
-                case 5: printf("Inverted half pyramid of numbers\n");
+        case 5: printf("[ Inverted half pyramid of numbers ]\n");
                 break;
 
-                case 6: printf("Full pyramid of *\n");
+        case 6: printf("[ Full pyramid of * ]\n");
                 break;
 
-                case 7: printf("Full pyramid of numbers\n");
+        case 7: printf("[ Full pyramid of numbers ]\n");
                 break;
 
-                case 8: printf("Inverted full pyramid of *\n");
+        case 8: printf("[ Inverted full pyramid of * ]\n");
                 break;
 
-                case 9: printf("Pascal's Triangle\n");
+        case 9: printf("[ Pascal's Triangle ]\n");
                 break;
 
-                case 10: printf("Floyd's Triangle\n");
-                break;
+        case 10: printf("[ Floyd's Triangle ]\n");
+                 break;
 
-                //default: printf("Choose another\n");
-                }
+        default: printf("Choose another\n");
+                 goto label;
         }
-
-    }
 
 return 0;
 }
