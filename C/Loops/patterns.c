9925dd8032c34abd83ac33af06c001c1d7b172b9
janforys <janforys@wp.pl>
2022-05-24 16:05:52 +0200

    'case 8' complete

--- C/Loops/patterns.c
+++ C/Loops/patterns.c
@@ -24,7 +24,8 @@ int main()
     switch (example) {
 
         case 1: printf("[ Half pyramid of * ]\n");
-                int half_star_row, half_star_col = 1;
+
+                int half_star_row, half_star_col;
                 printf("How many stars ?: ");
                 scanf("%d", &half_star_row);
                 printf("\n");
@@ -32,7 +33,7 @@ int main()
                 // Row iterator
                 for (int k = 0; k < half_star_row; k++) {
                         // Column iterator
-                        for (int j = 0; j < half_star_col; j++) {
+                        for (int j = 0; j <= half_star_col; j++) {
                                 printf("* ");
                             }
                         printf("\n");
@@ -42,7 +43,8 @@ int main()
 
 
         case 2: printf("[ Half pyramid of numbers ]\n");
-                int half_num_row, half_num_col = 1;
+
+                int half_num_row, half_num_col = 0;
                 printf("How many numbers ?: ");
                 scanf("%d", &half_num_row);
                 printf("\n");
@@ -51,7 +53,7 @@ int main()
                 for (int k = 0; k < half_num_row; k++) {
                         int num = 0;
                         // Column iterator
-                        for (int j = 0; j < half_num_col; j++) {
+                        for (int j = 0; j <= half_num_col; j++) {
                                 num++;
                                 printf("%d ", num);
                             }
@@ -62,7 +64,8 @@ int main()
 
 
         case 3: printf("[ Half pyramid of alphabets ]\n");
-                int half_alpha_row, half_alpha_col = 1, letter = 65;
+
+                int half_alpha_row, half_alpha_col = 0, letter = 65;
                 printf("How many letters ?: ");
                 scanf("%d", &half_alpha_row);
                 printf("\n");
@@ -70,7 +73,7 @@ int main()
                 // Row iterator
                 for (int k = 0; k < half_alpha_row; k++) {
                     // Column iterator
-                        for (int j = 0; j < half_alpha_col; j++) {
+                        for (int j = 0; j <= half_alpha_col; j++) {
                                 printf("%c ", letter);
                             }
                         printf("\n");
@@ -81,6 +84,7 @@ int main()
 
 
         case 4: printf("[ Inverted half pyramid of * ]\n");
+
                 int inv_star_row, inv_star_col;
                 printf("How many stars ?: ");
                 scanf("%d", &inv_star_row);
@@ -100,6 +104,7 @@ int main()
 
 
         case 5: printf("[ Inverted half pyramid of numbers ]\n");
+
                 int inv_num_row, inv_num_col;
                 printf("How many numbers ?: ");
                 scanf("%d", &inv_num_row);
@@ -121,33 +126,38 @@ int main()
 
 
         case 6: printf("[ Full pyramid of * ]\n");
-                int full_star_row, full_star_col = 1;
-                printf("How many stars ?: ");
-                scanf("%d", &full_star_row);
-                printf("\n");
-                full_star_col = full_star_row;
-
-                // Row iterator
-                for (int k = 0; k < full_star_row; k++) {
-                    for (int m = 0; m <= full_star_row / 2; m++) {
-                                    printf(" ");
-                                }
-                        // Column iterator
-                        for (int j = full_star_col; j >= 1; j--) {
-                                printf("* ");
-
-                            }
-                        printf("\n");
-                        full_star_col--;
-                    }
                 break;
-
 
         case 7: printf("[ Full pyramid of numbers ]\n");
                 break;
 
         case 8: printf("[ Inverted full pyramid of * ]\n");
+
+                int inv_full_star_row, inv_full_star_col, indentation = 0;
+                printf("How many stars ?: ");
+                scanf("%d", &inv_full_star_row);
+                printf("\n");
+                inv_full_star_col = inv_full_star_row;
+
+                // Row iterator
+                for (int r = 0; r < inv_full_star_row; r++) {
+                    int p = 0;
+                    // Column iterator
+                        for (int c = inv_full_star_col; c >= 1; c--) {
+                                printf("* ");
+                            }
+
+                        printf("\n");
+                        inv_full_star_col -= 2;
+                        indentation += 2;
+
+                            while (p < indentation) {
+                                    printf(" ");
+                                    p++;
+                                }
+                    }
                 break;
+
 
         case 9: printf("[ Pascal's Triangle ]\n");
                 break;
