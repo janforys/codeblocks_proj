559470b55b06e4a4a000f1b7c5d0abdba3bab135
janforys <janforys@wp.pl>
2022-06-07 10:59:25 +0200

    update

--- C/Loops/factorial.c
+++ C/Loops/factorial.c
@@ -2,18 +2,18 @@
 
 int main()
 {
-    double user_fac,count,factorial=1;
+    double user_fact, count, factorial = 1;
     printf("Type how many factorial of a number you want: ");
-    scanf("%lf",&user_fac);
+    scanf("%lf", &user_fact);
 
     //1*2*3*4*5*6... etc.
-    for (count=1; count<=user_fac; count++)
-    {
-        printf("%lf\n",count);
-        factorial*=count;
-    }
+    for (count = 1; count <= user_fact; count++)
+        {
+            printf("%lf\n", count);
+            factorial *= count;
+        }
 
-    printf("Result: %lf\n",factorial);
+    printf("Result: %lf\n", factorial);
 
     return 0;
 }
