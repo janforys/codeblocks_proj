23e15ee8d6c3f6435e576ff0306443f71ec8d06e
janforys <janforys@wp.pl>
2022-06-18 15:44:03 +0200

    Keyword 'long' Done.

--- C/Functions/long_keyword.c
+++ C/Functions/long_keyword.c
@@ -2,33 +2,34 @@
 
 
 #include <stdio.h>
-
-void checkNumber(int number) {
-    if (number < 2147483647 && number > -2147483647) {
-            printf("Nie jest to liczba typu long.");
-        }
-        else {
-            printf("Jest to liczba typu long.");
-        }
-}
+// Function declaration
+void checkNumber(long number);
 
 int main() {
 
     int a;
-    long b;
+    long b, number;
     long long c;
-    long number;
 
     printf("\nint = %zu BYTES (32 bits) \n", sizeof(a));
     printf("long = %zu BYTES (64 bits) \n", sizeof(b));
     printf("long long = %zu BYTES (64 bits) \n\n", sizeof(c));
 
-    printf("Podaj jakas ogromna liczbe... ");
+    printf("Type some enormous number... ");
     scanf("%ld", &number);
-    printf("%ld \n", number);
-
-    void checkNumber(int number);
+    // Function call. 'number' is passed to the function
+    checkNumber(number);
 
     return 0;
+}
+
+// Function definition
+void checkNumber(long number) {
+
+    if (number < 2147483647 && number > -2147483647) {
+        printf("It's not a 'long' type value.\n");
+    } else {
+        printf("It is 'long' type value.\n");
+    }
 }
 
