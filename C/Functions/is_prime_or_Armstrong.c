2afa200d4a551e87a9e0166262885a56d5675018
janforys <janforys@wp.pl>
2022-06-24 22:33:39 +0200

    Is prime number or an Armstrong or both Done

--- C/Functions/is_prime_or_Armstrong.c
+++ C/Functions/is_prime_or_Armstrong.c
@@ -1,51 +1,57 @@
-/* C Program to Check Prime or Armstrong Number Using User-defined Function */
+/* C Program to Check Prime or Armstrong user_number Using User-defined Function */
 
 
 #include <math.h>
 #include <stdio.h>
 
-void isArmstrong(int number);
-void isPrime(int number);
+void isArmstrong(int user_number);
+void isPrime(int user_number);
 
 int main() {
 
-    int number;
+    int user_number;
     printf("Number: ");
-    scanf("%d", &number);
+    scanf("%d", &user_number);
 
-    isArmstrong(number);
-    //isPrime(number);
+    isArmstrong(user_number);
+    isPrime(user_number);
 
     return 0;
 }
 
-void isArmstrong(int number) {
-    int remainder, power = 1, num = number, count = 0;
+void isArmstrong(int user_number) {
+    int num1 = user_number, num2 = user_number, result, power_factor = 0;
+        // Calculate power factor
         do {
-            number /= 10;
-            count++;
-        } while (number >= 1);
+            num1 /= 10;
+            power_factor++;
+        } while (num1 >= 1);
 
-        do {
-            remainder = num % 10;
-            for (int i = 1; i <= count; i++) {
+        // Sum of digits raised to power
+        while (num2 >= 1) {
+            int power = 1;
+            int remainder = num2 % 10;
+            for (int i = 1; i <= power_factor; i++) {
                     power *= remainder;
                 }
-            num /= 10;
-            printf("%d ", power);
-        } while (num >= 1);
+            num2 /= 10;
+            result += power;
+        }
+        if (result == user_number) {
+            printf("Is Armstrong \n");
+        }
 }
 
-void isPrime(int number) {
+void isPrime(int user_number) {
     int flag = 0;
-        for (int n = 2; n < number; n++) {
-                if (number % n == 0) {
+        for (int n = 2; n < user_number; n++) {
+                if (user_number % n == 0) {
                     flag = 1;
                     break;
                 }
         }
     if (flag == 0) {
-        printf("Is prime.\n");
+        printf("Is prime \n");
     }
 }
 
