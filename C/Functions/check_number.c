3a7b5d6880438ff99b4033ce38097af618b22505
janforys <janforys@wp.pl>
2022-06-28 22:31:59 +0200

    Checking if entered number can consist of 2 primes

--- C/Functions/check_number.c
+++ C/Functions/check_number.c
@@ -26,9 +26,26 @@ int main() {
 }
 
 int checkNumber(int user_number) {
-    int prime_1, prime_2, sum, flag = 0;
+    int prime_1, prime_2, sum, flag = 0, marker_1 = 0, marker_2 = 0;
 
-    // some code needed here...
+    for (prime_1 = 2; prime_1 < user_number; prime_1++) {
+        for (int i = 2; i < prime_1; i++) {
+            if (prime_1 % i == 0) {
+                marker_1 = 1;
+                break;
+            }
+        }
+        if (marker_1 == 0) {
+           for (prime_2 = 2; prime_2 < user_number; prime_2++) {
+               for (int j = 2; j < prime_2; j++) {
+                   if (prime_2 % j == 0) {
+                        marker_2 = 1;
+                        break;
+                   }
+               }
+           }
+        }
+    }
 
     return flag;
 }
