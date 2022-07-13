fd5a149612fccb0aed55293cb3cf32f8be0ee5b9
janforys <janforys@wp.pl>
2022-07-13 11:47:33 +0200

    Greatest common divisor Finished

--- C/Functions/greatest_common_divisor.c
+++ C/Functions/greatest_common_divisor.c
@@ -3,24 +3,29 @@
 
 #include "stdio.h"
 
-int GCD(int num1, int num2);
+int GCD(int user_number1, int user_number2);
 
 int main() {
-    int num1, num2;
+    int user_number1, user_number2;
     printf("Type two numbers: \n");
-    scanf("%d \n %d", &num1, &num2);
-    printf("Greatest Common Divisor: %d \n", GCD(num1, num2));
+    scanf("%d \n %d", &user_number1, &user_number2);
+    printf("Greatest Common Divisor: %d \n", GCD(user_number1, user_number2));
     return 0;
 }
 
-int GCD(int num1, int num2) {
-    int remainder = num2;
-        if (remainder != 0) {
-            remainder = num1 % num2;
-            printf("%d\n", remainder);
-            GCD(num2, remainder);
-            // something goes wrong here...
-            return remainder;
-        }
+int GCD(int user_number1, int user_number2) {
+    // Swap numbers if first is lower than second
+    if (user_number1 < user_number2) {
+        int t = user_number1;
+        user_number1 = user_number2;
+        user_number2 = t;
+    }
+    if (user_number2 != 0) {
+        int remainder = user_number1 % user_number2;
+            if (remainder == 0) {
+                return user_number2; // returned value
+            }
+        GCD(user_number2, remainder); // recursion
+    }
 }
 
