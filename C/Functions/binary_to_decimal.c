90afc986973404075bcde01daadc2400e5c2858c
janforys <janforys@wp.pl>
2022-07-25 14:41:14 +0200

    update

--- C/Functions/binary_to_decimal.c
+++ C/Functions/binary_to_decimal.c
@@ -37,16 +37,16 @@ int binary2decimal(int binary_value) {
 }
 
 int decimal2binary(int decimal_value) {
-    int multiplier = 1, sum = 0, sum_result = 0;
+    int m, multiplier = 1, sum = 0, sum_result = 0;
     // Count 'multiplier' value, needed for remainder multiplying
     for (int d = decimal_value; d > 1; d /= 2) {
         multiplier *= 10;
     }
-        int m1 = multiplier; // to avoid 'multiplier' change
+    m = multiplier; // to avoid 'multiplier' change
         while (decimal_value >= 1) {
-            sum += decimal_value % 2 * m1;
+            sum += decimal_value % 2 * m;
             decimal_value /= 2;
-            m1 /= 10;
+            m /= 10;
         } do { // converting to result value
             sum_result += sum % 10 * multiplier;
             sum /= 10;
