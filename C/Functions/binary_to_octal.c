5c2d5b12e6987f9d92328649b7d66a93cb2ec399
janforys <janforys@wp.pl>
2022-07-29 17:26:48 +0200

    OCT -> BIN

--- C/Functions/binary_to_octal.c
+++ C/Functions/binary_to_octal.c
@@ -1,21 +1,26 @@
 /* C Program to Convert Binary Number to Octal and vice-versa
 
 Instruction how to make such conversion:
-https://www.electrical4u.com/binary-to-octal-and-octal-to-binary-conversion/ */
+https://www.electrical4u.com/binary-to-octal-and-octal-to-binary-conversion/
+
+Jan Forys 2022 */
 
 
 #include "stdio.h"
 #include "math.h"
+//#include "binary_to_decimal.c"
 
 long binary2octal(long binary_value);
 int octal2binary(int octal_value);
+int decimal2binary(int decimal_value);
 
 int main() {
-    long binary_value, octal_value;
+    long binary_value;
+    int octal_value;
     scanf("%ld", &binary_value);
     printf("%ld \n\n", binary2octal(binary_value));
-    //scanf("%d", &octal_value);
-    //printf("%d \n", octal2binary(octal_value));
+    scanf("%d", &octal_value);
+    printf("%d \n", octal2binary(octal_value));
     return 0;
 }
 
@@ -50,4 +55,36 @@ long binary2octal(long binary_value) {
         multiplier /= 10;
     } while (sum_result >= 1);
     return result;
+}
+
+int octal2binary(int octal_value) {
+/* TODO: Change 'binary_digit' from string to number.
+         Put 'decimal2binary' function into separate library file. */
+    int binary_digit;
+    while (octal_value >= 1) {
+        binary_digit = decimal2binary(octal_value % 10);
+        octal_value /= 10;
+        printf("%d", binary_digit);
+    }
+    printf("%d\n");
+    return 777;
+}
+
+int decimal2binary(int decimal_value) {
+    int m, multiplier = 1, sum = 0, sum_result = 0;
+    // Count 'multiplier' value, needed for remainder multiplying
+    for (int d = decimal_value; d > 1; d /= 2) {
+        multiplier *= 10;
+    }
+    m = multiplier; // to avoid 'multiplier' change
+        while (decimal_value >= 1) {
+            sum += decimal_value % 2 * m;
+            decimal_value /= 2;
+            m /= 10;
+        } do { // converting to result value
+            sum_result += sum % 10 * multiplier;
+            sum /= 10;
+            multiplier /= 10;
+        } while (sum >= 1);
+    return sum_result;
 }
